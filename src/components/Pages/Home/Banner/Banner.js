db442bd3eb0efc80b76372fbbdd01c568e94f4f1
Torikul <toriqulislam142@gmail.com>
2022-05-22 02:49:22 -0400

    added banner section

---
+++ src/components/Pages/Home/Banner/Banner.js
@@ -0,0 +1,78 @@
+
+import React from "react";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import './Banner.css'
+
+const Banner = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  }
+  return (
+
+
+    <div className="banner-container">
+      <Carousel
+        additionalTransfrom={0}
+        arrows
+        autoPlaySpeed={30000}
+        centerMode={false}
+        className=""
+        containerClass="container-with-dots"
+        dotListClass=""
+        draggable
+        focusOnSelect={false}
+        infinite
+        itemClass=""
+        keyBoardControl
+        minimumTouchDrag={80}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={responsive}>
+        <div className="banner-item1">
+          <div className="item-content">
+            <h1 className="text-5xl ">YOUR CAR SAVE OUR HAND !</h1>
+            <p className="py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ut optio soluta. Deleniti voluptatem ratione error animi ducimus odit minima facilis esse delectus quas earum distinctio, necessitatibus voluptatum dolore ipsa?</p>
+            <button className="btn btn-primary">SIGN UP NOW</button>
+          </div>
+        </div>
+
+
+        <div className="banner-item2">
+        <div className="item-content">
+            <h1 className="text-5xl ">YOUR CAR SAVE OUR HAND !</h1>
+            <p className="py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ut optio soluta. Deleniti voluptatem ratione error animi ducimus odit minima facilis esse delectus quas earum distinctio, necessitatibus voluptatum dolore ipsa?</p>
+            <button className="btn btn-primary">SIGN UP NOW</button>
+          </div>
+        </div>
+
+        <div className="banner-item3">
+        <div className="item-content">
+            <h1 className="text-5xl ">YOUR CAR SAVE OUR HAND !</h1>
+            <p className="py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ut optio soluta. Deleniti voluptatem ratione error animi ducimus odit minima facilis esse delectus quas earum distinctio, necessitatibus voluptatum dolore ipsa?</p>
+            <button className="btn btn-primary">SIGN UP NOW</button>
+          </div>
+        </div>
+
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Banner from './Banner';
+import Banner from './Banner/Banner';
+
 
 const Home = () => {
     return (
