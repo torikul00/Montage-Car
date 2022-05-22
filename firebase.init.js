e310bae3de1a4af981ba7118f7cdaa3884518983
Torikul <toriqulislam142@gmail.com>
2022-05-22 15:11:14 -0400

    install firebase and firebase hooks and initialize

---
+++ firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDM0uBOCCrPSgQIbcJiTQ6SRN7QD5X9Fm0",
+  authDomain: "montage-car.firebaseapp.com",
+  projectId: "montage-car",
+  storageBucket: "montage-car.appspot.com",
+  messagingSenderId: "143228911909",
+  appId: "1:143228911909:web:2fdf8e212d3538e9dbdacc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
